b8c0654deab1b1618b3441f669d012a56d8f31e0
bljboy <bljboy55@gmail.com>
2023-12-19 15:34:42 +0800

    mmkv初体验

--- app/src/main/java/com/example/clouds/MainActivity.java
+++ app/src/main/java/com/example/clouds/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewbinding.ViewBinding;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.clouds.base.BaseActivity;
 import com.example.clouds.databinding.ActivityMainBinding;
+import com.tencent.mmkv.MMKV;
 
 public class MainActivity extends BaseActivity<ActivityMainBinding> {
+    private final String TAG = getClass().getName();
 
     @Override
     protected ActivityMainBinding getViewBinding() {
@@ -18,6 +23,17 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
     @Override
     protected void initView() {
         mBinding.text.setText("你是谁");
+        MMKV.initialize(this);
+        MMKV mmkv = MMKV.defaultMMKV();
+        mmkv.encode("name", 123);
+        mmkv.apply();
+        mBinding.mmkv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int name = mmkv.decodeInt("name");
+                Log.d(TAG, "onClick: " + name);
+            }
+        });
     }
 
     @Override
